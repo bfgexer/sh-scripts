c0c44da13576b61f6f00ad3c74db8373b564f781
nabegator <bvc29713@gmail.com>
2025-05-07 22:38:17 +0500

    Добавлено выполнение установки и настройки Nginx в скрипте установки

--- scripts/nginx_install.sh
+++ scripts/nginx_install.sh
@@ -1,3 +1,13 @@
+sudo apt update
+sudo apt install nginx -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
+#открываем порты:
+sudo ufw allow 'Nginx Full'
+sudo ufw status verbose
+
+
+
 echo "скрипт скачался и запустился"
 SCRIPT_PATH="$(realpath "$0")"
 echo "🧹 Удаляю скрипт: $SCRIPT_PATH"
