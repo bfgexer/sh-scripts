ab497ac0b0ed5a3faea035afd347a175fc3e4b31
nabegator <bvc29713@gmail.com>
2025-05-11 02:01:56 +0500

    Добавлено включение и настройка fail2ban в скрипте установки

--- scripts/fail2ban_install.sh
+++ scripts/fail2ban_install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 
-sudo apt update && sudo apt install fail2ban 
+sudo apt update && sudo apt install fail2ban
+sudo systemctl enable fail2ban 
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+sudo sed -i '/^\[sshd\]/a enabled = true\nmaxretry = 3\nbantime = 1h\nfindtime = 10m' /etc/fail2ban/jail.local
+sudo sed -i '/^\[nginx-http-auth\]/a enabled = true\nmaxretry = 3\nbantime = 1h\nfindtime = 10m' /etc/fail2ban/jail.local
+sudo systemctl restart fail2ban
 
 
 
