55b7ac22b548adba82a9e5c866565ec41a6c2e31
nabegator <bvc29713@gmail.com>
2025-05-11 18:08:04 +0500

    Добавлено ключ -y к установке Certbot в скрипте установки Nginx для автоматического подтверждения

--- scripts/nginx_install.sh
+++ scripts/nginx_install.sh
@@ -28,10 +28,9 @@ server {
     }
 }
 EOF
-
 sudo ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/
 sudo nginx -t && sudo systemctl reload nginx
-sudo apt install certbot python3-certbot-nginx
+sudo apt install certbot python3-certbot-nginx -y
 sudo certbot --nginx -d $domain
 sudo certbot renew --dry-run
 
