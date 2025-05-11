cf9f92214a4b3616d4b7657cd41f956204223f53
nabegator <bvc29713@gmail.com>
2025-05-11 18:03:40 +0500

    Убраны комментарии для установки и настройки Certbot в скрипте установки Nginx

--- scripts/nginx_install.sh
+++ scripts/nginx_install.sh
@@ -31,11 +31,11 @@ EOF
 
 sudo ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/
 sudo nginx -t && sudo systemctl reload nginx
-# sudo apt install certbot python3-certbot-nginx
-# sudo certbot --nginx -d $domain
-# sudo certbot renew --dry-run
+sudo apt install certbot python3-certbot-nginx
+sudo certbot --nginx -d $domain
+sudo certbot renew --dry-run
 
-# sudo nginx -t && sudo systemctl reload nginx
+sudo nginx -t && sudo systemctl reload nginx
 echo "скрипт скачался и запустился"
 SCRIPT_PATH="$(realpath "$0")"
 echo "🧹 Удаляю скрипт: $SCRIPT_PATH"
