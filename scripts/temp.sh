430a52c542cc9ebb066bcd301dfa76362cde57cc
nabegator <bvc29713@gmail.com>
2025-05-21 16:50:50 +0500

    Удален лишний текст из скрипта temp.sh

--- scripts/temp.sh
+++ scripts/temp.sh
@@ -4,3 +4,4 @@ sudo chown -R www-data:www-data /var/www/finishing.nabegator3000.site
 sudo chmod -R 755 /var/www/finishing.nabegator3000.site
 
 sudo apt update && sudo apt install php8.3 php8.3-fpm php8.3-mysql php8.3-zip php8.3-xml php8.3-mbstring php8.3-curl -y
+dfgdfg
