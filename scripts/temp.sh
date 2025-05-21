67daecb558f911dc6e6cbb94ddc5586b355e9990
nabegator <bvc29713@gmail.com>
2025-05-21 16:48:23 +0500

    modified:   scripts/temp.sh

--- scripts/temp.sh
+++ scripts/temp.sh
@@ -2,3 +2,5 @@ sudo mkdir -p /var/www/finishing.nabegator3000.site/html
 sudo chown -R nabegator3000:nabegator3000 /var/www/finishing.nabegator3000.site/html
 sudo chown -R www-data:www-data /var/www/finishing.nabegator3000.site
 sudo chmod -R 755 /var/www/finishing.nabegator3000.site
+
+sudo apt update && sudo apt install php8.3 php8.3-fpm php8.3-mysql php8.3-zip php8.3-xml php8.3-mbstring php8.3-curl -y
