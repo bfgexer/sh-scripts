57c179517daf5a284d0361f75a8a7a1eb5bc9ab9
nabegator <bvc29713@gmail.com>
2025-05-21 12:37:57 +0500

    Добавлен скрипт для создания и настройки каталога веб-сайта finishing.nabegator3000.site

--- scripts/nginx_install.sh
+++ scripts/nginx_install.sh
@@ -31,11 +31,11 @@ EOF
 sudo ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/
 sudo nginx -t && sudo systemctl reload nginx
 sudo apt install certbot python3-certbot-nginx -y
-sudo certbot --nginx -d $domain
-sudo certbot renew --dry-run
+# sudo certbot --nginx -d $domain
+# sudo certbot renew --dry-run
 
-sudo chmod 755 /etc/letsencrypt/live
-sudo chmod 755 /etc/letsencrypt/archive
+# sudo chmod 755 /etc/letsencrypt/live
+# sudo chmod 755 /etc/letsencrypt/archive
 
 sudo nginx -t && sudo systemctl reload nginx
 echo "скрипт скачался и запустился"
---
+++ scripts/temp.sh
@@ -0,0 +1,4 @@
+sudo mkdir -p /var/www/finishing.nabegator3000.site/html
+sudo chown -R nabegator3000:nabegator3000 /var/www/finishing.nabegator3000.site/html
+sudo chown -R www-data:www-data /var/www/finishing.nabegator3000.site
+sudo chmod -R 755 /var/www/finishing.nabegator3000.site
